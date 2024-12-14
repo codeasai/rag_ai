2ec63d34d75c7e2092bb087ec032b1b9d1f03dd8
Surasak Suksathit <cybergig@gmail.com>
2024-12-14 22:05:12 +0700

    Update READMe

--- tests/clean_nullbytes_utils.py
+++
@@ -1,9 +0,0 @@
-# Before running this code, PLEASE ensure you have a backup of the original file "test_utils.py"
-
-with open('test_utils.py', 'r') as original_file:
-    data = original_file.read()
-
-cleaned_data = data.replace('\x00', '')  # Removing all null bytes
-
-with open('cleaned_test_utils.py', 'w') as cleaned_file:
-    cleaned_file.write(cleaned_data)
